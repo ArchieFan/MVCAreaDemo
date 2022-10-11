412819e836409759f3ad3f2c823e086b37fadceb
Archie Fan <archiefan@gmail.com>
2022-10-11 20:16:47 +0100

    Add project files.

---
+++ MVCAreaDemo/Models/EmployeeMetaData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCAreaDemo.Models
+{
+    [MetadataType(typeof(EmployeeMetaData))]
+    public partial class Employee
+    {
+    }
+
+    public class EmployeeMetaData
+    {
+        [StringLength(10,MinimumLength =5, ErrorMessage = "Custom Message - Min Length 5 and Max Length is 10")]
+        [Required]
+        public string Name { get; set; }
+    }
+}
